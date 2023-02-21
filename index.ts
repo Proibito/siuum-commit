95a53ded3e9188bd5e01ea5cf40639158639fd26
Edoardo <edoardobalzano01@gmail.com>
2023-02-21 14:51:06 +0100

    header

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
+#!/usr/bin/env node
+
 import { sleep } from "bun";
 import chalk from "chalk";
 import { Command } from 'commander';
 import { ResetMode, simpleGit, SimpleGit, SimpleGitOptions } from 'simple-git';
+import inquirer, { PromptModule, QuestionCollection } from 'inquirer';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
 
 const program = new Command();
 
@@ -14,12 +21,45 @@ const options: Partial<SimpleGitOptions> = {
 
 const git: SimpleGit = simpleGit(options);
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const domandeIT: QuestionCollection = [
+    {
+        type: 'list',
+        name: 'type',
+        message: 'Che cosa vuoi fare?',
+        choices: ['docs', 'feat', 'fix', 'pref', 'refactor', 'test', 'custom'],
+        loop: true,
+
+        // filter(val) {
+        //     return val.toLowerCase();
+        // },
+    }
+]
+
 program
-    .command("commit")
     .action(async () => {
-        git.commit("sium")
+        console.log(process.cwd());
+
+        let messaggio = "";
+
+        await inquirer.prompt(domandeIT)
+            .then((rispota) => {
+                messaggio = `${rispota.type}:`
+
+            })
+        console.log(chalk.green("Commit effettuato"));
+        git.commit("header")
+        git.commit("body")
 
     });
 
+program
+    .command("init")
+    .action(() => {
+        console.log("ciao");
+
+    })
 
 program.parse(process.argv);
